bb160915a323e210c2093b36d0320dfbd2a2deb3
perteadi <adi@softhaus.org>
2019-04-28 00:43:23 +0300

    init

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -3,10 +3,16 @@ import Vapor
 /// Controls basic CRUD operations on `Todo`s.
 final class TodoController {
     /// Returns a list of all `Todo`s.
-    func index(_ req: Request) throws -> Future<[Todo]> {
+    func allObjects(_ req: Request) throws -> Future<[Todo]> {
         return Todo.query(on: req).all()
     }
-
+    
+    func getById(_ req: Request, id: Int) throws -> Future<Todo> {
+        return Todo.find(id, on: req).map(to: Todo.self) { (post) in
+            guard let post = post else {throw Abort(HTTPStatus.notFound)}
+            return post
+        }
+    }
     /// Saves a decoded `Todo` to the database.
     func create(_ req: Request) throws -> Future<Todo> {
         return try req.content.decode(Todo.self).flatMap { todo in
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -14,10 +14,18 @@ public func routes(_ router: Router) throws {
     router.get("f"){ req in
         return "Fuck"
     }
-
+    
+    router.get("todos","id", Int.parameter) { req -> EventLoopFuture<Todo> in
+        let id = try req.parameters.next(Int.self)
+        let todoController = TodoController()
+        return try todoController.getById(req, id: id)
+    }
+    
     // Example of configuring a controller
     let todoController = TodoController()
-    router.get("todos", use: todoController.index)
+    router.get("todos", use: todoController.allObjects)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+
+
 }
